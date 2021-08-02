2689815dd80093263ae57a810ff76a479ef14772
ShaneMaglangit <shanedmaglangit@gmail.com>
2021-08-02 23:48:50 +0800

    Add missing color

--- agp.go
+++ agp.go
@@ -58,7 +58,6 @@ func ParseHex(hex string) (*GeneBinGroup, error) {
 	// Create the GeneBinGroup object with the parsed binary string.
 	return &GeneBinGroup{
 		Class:    bStr[0:4],
-		Reserved: bStr[4:8],
 		Region:   bStr[8:13],
 		Tag:      bStr[13:18],
 		BodySkin: bStr[18:22],
@@ -208,15 +207,15 @@ func getPatternGenes(gbg *GeneBinGroup) (*PatternGene, error) {
 }
 
 var classColorMap = map[Class]map[string]string{
-	Beast:   {"0010": "ffec51", "0011": "ffa12a", "0100": "f0c66e", "0110": "60afce"},
-	Bug:     {"0010": "ff7183", "0011": "ff6d61", "0100": "f74e4e"},
-	Bird:    {"0010": "ff9ab8", "0011": "ffb4bb", "0100": "ff778e"},
-	Plant:   {"0010": "ccef5e", "0011": "efd636", "0100": "c5ffd9"},
-	Aquatic: {"0010": "4cffdf", "0011": "2de8f2", "0100": "759edb", "0110": "ff5a71"},
-	Reptile: {"0010": "fdbcff", "0011": "ef93ff", "0100": "f5e1ff", "0110": "43e27d"},
-	Mech:    {"0010": "D9D9D9", "0011": "D9D9D9", "0100": "D9D9D9", "0110": "D9D9D9"},
-	Dusk:    {"0010": "D9D9D9", "0011": "D9D9D9", "0100": "D9D9D9", "0110": "D9D9D9"},
-	Dawn:    {"0010": "D9D9D9", "0011": "D9D9D9", "0100": "D9D9D9", "0110": "D9D9D9"},
+	Beast:   {"0010": "ffec51", "0011": "ffa12a", "0100": "f0c66e", "0110": "60afce", "0000": "ffffff"},
+	Bug:     {"0010": "ff7183", "0011": "ff6d61", "0100": "f74e4e", "0000": "ffffff"},
+	Bird:    {"0010": "ff9ab8", "0011": "ffb4bb", "0100": "ff778e", "0000": "ffffff"},
+	Plant:   {"0010": "ccef5e", "0011": "efd636", "0100": "c5ffd9", "0000": "ffffff"},
+	Aquatic: {"0010": "4cffdf", "0011": "2de8f2", "0100": "759edb", "0110": "ff5a71", "0000": "ffffff"},
+	Reptile: {"0010": "fdbcff", "0011": "ef93ff", "0100": "f5e1ff", "0110": "43e27d", "0000": "ffffff"},
+	Mech:    {"0010": "D9D9D9", "0011": "D9D9D9", "0100": "D9D9D9", "0110": "D9D9D9", "0000": "ffffff"},
+	Dusk:    {"0010": "D9D9D9", "0011": "D9D9D9", "0100": "D9D9D9", "0110": "D9D9D9", "0000": "ffffff"},
+	Dawn:    {"0010": "D9D9D9", "0011": "D9D9D9", "0100": "D9D9D9", "0110": "D9D9D9", "0000": "ffffff"},
 }
 
 // getColorGenes extracts the color genes from the gene binary group. It will return one dominant and two recessive
